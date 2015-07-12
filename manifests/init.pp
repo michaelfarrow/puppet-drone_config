01c6ddb05dcc9214ee6e30a2722ad31f8734149c
Mike Farrow <contact@mikefarrow.co.uk>
2015-07-12 20:59:10 +0100

    adding service and fixing typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,16 @@ class drone_config (
 	$config_content = ''
 ) {
 
+	service { "droned":
+		ensure => running,
+		require => Package ['drone']
+	}
+
 	file { "/etc/drone/drone.toml":
 		ensure => present,
 		content => $config_content,
-		require => package['drone']
+		require => Package ['drone'],
+		notify => Service ['droned']
 	}
 
 }
