480ca9f2ed38d13c97184a14fe62ee662a7a17af
Mike Farrow <contact@mikefarrow.co.uk>
2015-07-12 21:55:12 +0100

    adding volume cleanup cron job

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,14 @@ class drone_config (
 
 	Class ['docker'] ~> Class ['drone']
 
+	cron { "cleanup docker volumes":
+		command => "docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes",
+		user => root,
+		hour => "*",
+		minute => 5,
+		require => Package ['docker']
+	}
+
 	service { "drone":
 		ensure => running,
 		require => Package ['drone']
